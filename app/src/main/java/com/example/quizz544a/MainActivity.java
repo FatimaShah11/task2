bf383c977da5a4e2eed8856660785b3d8b08e191
FatimaShah11 <ruhishah137@gmail.com>
2021-11-09 03:09:46 +0500

    url

--- app/src/main/java/com/example/quizz544a/MainActivity.java
+++ app/src/main/java/com/example/quizz544a/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quizz544a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,15 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button startbutton=(Button)findViewById(R.id.button);
+        Button url=(Button)findViewById(R.id.urlbtn);
         final EditText nametext=(EditText)findViewById(R.id.editName);
 
         startbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name=nametext.getText().toString();
-                //Intent intent=new Intent(getApplicationContext(),QuestionsActivity.class);
-                //intent.putExtra("myname",name);
-                //startActivity(intent);
+                Intent intent=new Intent(getApplicationContext(),QuestionsActivity.class);
+                intent.putExtra("myname",name);
+                startActivity(intent);
+            }
+        });
+        url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://github.com/FatimaShah11/task2";
+                Intent intent1=new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse(url));
+                startActivity(intent1);
             }
         });
 
