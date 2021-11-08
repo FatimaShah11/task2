c6ae3334fce905a0e44aff7d4dbb1b4cd4ec2ed0
FatimaShah11 <ruhishah137@gmail.com>
2021-11-09 02:12:47 +0500

    code

--- app/src/main/java/com/example/quizz544a/MainActivity.java
+++ app/src/main/java/com/example/quizz544a/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.quizz544a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button startbutton=(Button)findViewById(R.id.button);
+        final EditText nametext=(EditText)findViewById(R.id.editName);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=nametext.getText().toString();
+                //Intent intent=new Intent(getApplicationContext(),QuestionsActivity.class);
+                //intent.putExtra("myname",name);
+                //startActivity(intent);
+            }
+        });
+
     }
 }
