4a57e0d147d5bf7aa2ce3c34f252469104ac41e8
FatimaShah11 <ruhishah137@gmail.com>
2021-12-04 21:37:59 +0500

    UpAction

--- app/src/main/java/com/example/quizz544a/MainActivity.java
+++ app/src/main/java/com/example/quizz544a/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.quizz544a;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Quiz");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button startbutton=(Button)findViewById(R.id.button);
         Button url=(Button)findViewById(R.id.urlbtn);
