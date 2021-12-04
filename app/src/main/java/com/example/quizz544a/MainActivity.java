537fe8c3efd408a60d1f9ced233fdab1b03d78fa
FatimaShah11 <ruhishah137@gmail.com>
2021-12-04 21:04:07 +0500

    SetSupportToolBar

--- app/src/main/java/com/example/quizz544a/MainActivity.java
+++ app/src/main/java/com/example/quizz544a/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quizz544a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Quiz");
 
         Button startbutton=(Button)findViewById(R.id.button);
         Button url=(Button)findViewById(R.id.urlbtn);
